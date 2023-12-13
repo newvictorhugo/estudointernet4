982b4cfeb3db151fd57420015fa7120cb2bbf4a3
Victor Hugo <victorhugo10.2003@hotmail.com>
2023-12-13 20:53:57 -0300

    feats: validacao login

---
+++ index.js
@@ -0,0 +1,308 @@
+//Módulo 4 deverá acrescentar duas novas funcionalidades ao nosso projeto
+//Exibir a data do último acesso ao usuário (cookies)
+// Autenticar o usuário para controlar o acesso aos recursos da aplicação (sessão)
+
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+
+const porta = 3000;
+const host = '0.0.0.0';
+
+var listaUsuarios = [];
+
+function processarCadastroUsuario(requisicao, resposta){
+    // extrair os dados do corpo da requisição, além de validar os dados
+    const dados = requisicao.body;
+    let conteudoResposta = '';
+    //é necessário validar os dados enviados
+    //a validação dos dados é de responsabilidade da aplicação do servidor
+    if(!(dados.nome && dados.sobrenome && dados.nomeUsuario && dados.cidade && dados.UF && dados.cep)){
+        // estão faltando dados do usuário!
+        conteudoResposta = `
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <title>Document</title>
+        </head>
+        <body>
+            
+            <div class="container">
+                <form action="/cadastrarUsuario" method="post" class="row g-3 needs-validation" novalidate>
+                    <fieldset class="border p-2">
+                        <legend class="mb-3">Cadastro de Usuário</legend>
+                        <div class="col-md-4">
+                        <label for="" class="form-label">Nome</label>
+                        <input type="text" class="form-control" id="nome" name="nome" value="${dados.nome}" required>
+                        </div>
+        `;
+        if(!dados.nome){
+            conteudoResposta+=`
+                <div>
+                    <p class="text-danger">Por favor, informe o nome</p>
+                </div>
+            `
+        }
+        conteudoResposta+=`
+        <div class="col-md-4">
+            <label for="sobrenome" class="form-label">Sobrenome</label>
+            <input type="text" class="form-control" id="sobrenome" name="sobrenome" value="${dados.sobrenome}" required>
+        </div>
+        `;
+        if(!dados.sobrenome){
+            conteudoResposta+=`
+                <div>
+                    <p class="text-danger">Por favor, informe o sobrenome</p>
+                </div>
+            `
+        }
+        conteudoResposta+=`
+        <div class="col-md-4">
+            <label for="nomeUsuario" class="form-label">Nome Usuário</label>
+            <input type="text" class="form-control" id="nomeUsuario" name="nomeUsuario" aria-describedby="inputGroupPrepend" value="${dados.nomeUsuario}" required>
+        </div>
+        `;
+        if(!dados.nomeUsuario){
+            conteudoResposta+=`
+                <div>
+                    <p class="text-danger">Por favor, informe o nome de usuario</p>
+                </div>
+            `
+        }
+        conteudoResposta+=`
+        <div class="col-md-6">
+            <label for="cidade" class="form-label">Cidade</label>
+            <input type="text" class="form-control" id="cidade" name="cidade" value="${dados.cidade}" required>
+        </div>
+        `;
+        if(!dados.cidade){
+            conteudoResposta+=`
+                <div>
+                    <p class="text-danger">Por favor, informe o nome da cidade</p>
+                </div>
+            `
+        }
+        conteudoResposta+=`
+        <div class="col-md-3">
+            <label for="uf" class="form-label">UF</label>
+            <select class="form-select" id="UF" name="UF" value="${dados.UF}" required>
+                <option selected disabled value="">Escolha um estado...</option>
+                <option value="AC">Acre</option>
+                <option value="AL">Alagoas</option>
+                <option value="AP">Amapá</option>
+                <option value="AM">Amazonas</option>
+                <option value="BA">Bahia</option>
+                <option value="CE">Ceará</option>
+                <option value="DF">Distrito Federal</option>
+                <option value="ES">Espírito Santo</option>
+                <option value="GO">Goiás</option>
+                <option value="MA">Maranhão</option>
+                <option value="MT">Mato Grosso</option>
+                <option value="MS">Mato Grosso do Sul</option>
+                <option value="MG">Minas Gerais</option>
+                <option value="PA">Pará</option>
+                <option value="PB">Paraíba</option>
+                <option value="PR">Paraná</option>
+                <option value="PE">Pernambuco</option>
+                <option value="PI">Piauí</option>
+                <option value="RJ">Rio de Janeiro</option>
+                <option value="RN">Rio Grande do Norte</option>
+                <option value="RS">Rio Grande do Sul</option>
+                <option value="RO">Rondônia</option>
+                <option value="RR">Roraima</option>
+                <option value="SC">Santa Catarina</option>
+                <option value="SP">São Paulo</option>
+                <option value="SE">Sergipe</option>
+                <option value="TO">Tocantins</option>
+            </select>
+        </div>
+        `;
+        if(!dados.UF){
+            conteudoResposta+=`
+                <div>
+                    <p class="text-danger">Por favor, informe o nome da UF</p>
+                </div>
+            `
+        }
+        conteudoResposta+=`
+        <div class="col-md-3">
+            <label for="cep" class="form-label">CEP</label>
+            <input type="text" class="form-control" id="cep" name="cep" value="${dados.cep}" required>
+        </div>
+        `;
+        if(!dados.cep){
+            conteudoResposta+=`
+                <div>
+                    <p class="text-danger">Por favor, informe o nome</p>
+                </div>
+            `
+        }
+        conteudoResposta+=`
+                <div class="col-12 mt-2">
+                        <button class="btn btn-primary" type="submit">Cadastrar</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        </body>
+        </html>
+        `;
+        resposta.end(conteudoResposta);
+    }else{
+    const usuario = {
+                        nome: dados.nome,
+                        sobrenome: dados.sobrenome,
+                        nomeUsuario: dados.nomeUsuario,
+                        cidade: dados.cidade,
+                        UF: dados.UF,
+                        cep: dados.cep
+                    }
+    //Adiciona um novo usuário na lista de usuários já cadastrados
+    listaUsuarios.push(usuario);
+    // returnar a lista de usuários
+    let conteudoResposta =`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Lista de usuários cadastrados</h1>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Sobrenome</th>
+                        <th>Nome Usuário</th>
+                        <th>Cidade</th>
+                        <th>UF</th>
+                        <th>CEP</th>
+                    </tr>
+                </thead>
+                <tbody>`;
+                for(const usuario of listaUsuarios){
+                    conteudoResposta += `
+                        <tr>
+                            <td>${usuario.nome}</td>
+                            <td>${usuario.sobrenome}</td>
+                            <td>${usuario.nomeUsuario}</td>
+                            <td>${usuario.cidade}</td>
+                            <td>${usuario.UF}</td>
+                            <td>${usuario.cep}</td>
+                            
+                        </tr>
+                    `
+                }
+
+                conteudoResposta+=`
+                </tbody>
+            </table>
+            <a class="btn btn-primary" href="/" role="button">Voltar para o menu</a>
+            <a class="btn btn-primary" href="/cadastrarUsuario.html" role="button">Cadastrar usuario</a>
+        </tbody>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        </html>
+                `;
+    resposta.end(conteudoResposta);
+    }//fim do if/else
+}
+
+//pseudo middleware
+function autenticar(requisicao, resposta, next){
+    if(requisicao.session.usuarioAutenticado){
+        next();
+    }else{
+        resposta.redirect("/login.html");
+    }
+}
+const app = express();
+//ativando a funcionalidade de manipular cookies
+app.use(cookieParser());
+
+// adicionar uma nova capacidade para essa aplicação: memorizar com quem o servidor está falando
+// durante o uso do sistema, a aplicação saberá, dentro da uma sessão válida, com quem ela se comunica.
+app.use(session({
+    secret:"M1nh4Ch4v353cr3t4",
+    resave: true, //atualiza a sessão mesmo que não há alterações a cada requisição
+    saveUninitialized: true,
+    cookie:{
+        //tempo de vida da sessão
+        maxAge: 1000*60*15 //15 minutos
+    }
+}));
+
+//ativar a extensão que manipula requisições HTTP
+//opção false ativa a extensão querystring
+//opção true ativa a extensão qs (manipula objetos (lista, alinhados))
+app.use(express.urlencoded({extended: true}));
+
+//Indicando para a aplicação como servir arquivos estáticos localizados na pasta 'paginas'
+app.use(express.static('paginas'));
+
+app.get('/', autenticar, (requisicao, resposta) =>{
+    const dataUltimoAcesso = requisicao.cookies.DataUltimoAcesso;
+    const data = new Date();
+    resposta.cookie("DataUltimoAcesso", data.toLocaleString(), {
+        maxAge: 1000*60*60*24*30,
+        httpOnly: true
+    })
+    resposta.end(`
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Menu do sistema</title>
+    </head>
+    <body>
+        <h1>Menu</h1>
+        <ul>
+            <li><a href="/cadastrarUsuario.html"><button type="button" class="btn btn-primary">Primary</button></a></li>
+        </ul>
+    </body>
+    <footer>
+        <p>Seu último acesso foi em ${dataUltimoAcesso}</p>
+    </footer>
+    </html>
+    `)
+})
+
+//endpoint login que irá processar o login da aplicação
+app.post('/login', (requisicao, resposta)=>{
+    const usuario = requisicao.body.usuario;
+    const senha = requisicao.body.senha;
+    if(usuario && senha && (usuario === 'victor') && (senha === '123')){
+        requisicao.session.usuarioAutenticado = true;
+        resposta.redirect('/');
+    }else{
+        resposta.end(`
+            <!DOCTYPE html>
+            <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Falha na autenticação</title>
+            </head>
+            <body>
+                <h3>Usuário ou senha inválidos!!</h3>
+                <a href="/login.html">Voltar para o login</a>
+            </body>
+            </html>
+        `)
+    }
+})
+
+// rota para processar o cadastro de usuários endpoint = '/cadastrarUsuario'
+
+app.post('/cadastrarUsuario', autenticar, processarCadastroUsuario);
+
+app.listen(porta, host, () =>{
+    console.log(`Servidor executando na URL http://${host}:${porta}`);
+})
